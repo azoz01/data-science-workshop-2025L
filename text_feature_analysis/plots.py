0b260dd30579b163a30e9848d52d4aa25b8215b3
wpartycja <w512patrycja@gmail.com>
2025-03-31 11:31:35 +0200

    new text features analysis part

--- text_feature_analysis/plots.py
+++ text_feature_analysis/plots.py
@@ -3,6 +3,23 @@ import seaborn as sns
 import matplotlib.patches as mpatches
 import pandas as pd
 import numpy as np
+from typing import Tuple
+
+group_colors = {
+    "gpt": ("blue", 1),
+    "claude": ("orange", 2),
+    "mixtral": ("green", 3),
+    "mistral": ("green", 3),
+    "qwen": ("red", 4),
+    "llama": ("purple", 5)
+}
+
+def get_group_and_color(name: str) -> Tuple[int, str]:
+    name = name.lower()
+    for group, (color, order) in group_colors.items():
+        if group in name:
+            return order, color
+    return 99, "gray"  # Default if no match
 
 
 def plot_features_distribution(
@@ -10,11 +27,13 @@ def plot_features_distribution(
     female_df: pd.DataFrame,
     all_pvalues: dict,
     pvalue: float,
+    model_name: str,
     test_name: str = "Wilcoxon",
     max_cols: int = 5,
     base_width: int = 3,
     base_height: int = 3,
     path_to_save: str = None,
+    show: bool = True
 ) -> None:
     # dynamically caluclate number of rows and columns
     num_features = len(all_pvalues)
@@ -27,9 +46,8 @@ def plot_features_distribution(
     fig, axes = plt.subplots(num_rows, num_cols, figsize=(fig_width, fig_height))
 
     fig.suptitle(
-        "Distributions of text features across genders, \n"
-        f"{test_name} test, p-value < {pvalue}",
-        fontsize=16,
+        f"Distributions of text features across genders in {model_name}, \n"
+        f"{test_name} test, p-value < {pvalue}"
     )
 
     # flattering axes for easy iteration
@@ -56,16 +74,20 @@ def plot_features_distribution(
 
     # save and show
     if path_to_save:
-        plt.savefig(path_to_save)
-    plt.show()
+        plt.savefig(path_to_save+f'/{model_name}.png')
+        
+    plt.show() if show else plt.close()
 
 
-def plot_mean_differences(sorted_means: dict, path_to_save: str = None) -> None:
+def plot_mean_differences(sorted_means: dict, model_name: str, path_to_save: str = None, show: bool = True, across_models: bool = False) -> None:
 
     colors = ["skyblue" if val > 0 else "hotpink" for val in sorted_means.values()]
 
     # figure and plot
-    fig, ax = plt.subplots(figsize=(10, 18))
+    figsize = (4, 6) if across_models else (12, 18)
+    
+    fig, ax = plt.subplots(figsize=figsize)
+
     sns.barplot(
         y=list(sorted_means.keys()),
         x=list(sorted_means.values()),
@@ -75,7 +97,6 @@ def plot_mean_differences(sorted_means: dict, path_to_save: str = None) -> None:
     )
     ax.set_xlabel("Normalized Mean Difference (male - female)")
     ax.set_ylabel("Columns")
-    ax.set_title("Normalized Differences Between Means")
     ax.grid()
 
     # legend and title
@@ -92,10 +113,38 @@ def plot_mean_differences(sorted_means: dict, path_to_save: str = None) -> None:
         ncol=2,
         bbox_to_anchor=(0.0, 1.01, 1.0, 0.102),
     )
-    ax.set_title("Normalized Differences Between Means", pad=40)
+    ax.set_title(f"Normalized Differences Between Means in {model_name}", pad=40)
 
     # save and show
     if path_to_save:
-        plt.savefig()
+        plt.savefig(path_to_save+f'/{model_name}.png')
+
+    plt.show() if show else plt.close()
+
+
+def plot_total_means_differences(total_mean_diff: dict) -> None:
+    
+    models = list(total_mean_diff.keys())
+    values = list(total_mean_diff.values())
+    
+    model_data = [(model, values[i], *get_group_and_color(model)) for i, model in enumerate(models)]
+    model_data.sort(key=lambda x: x[2])  
+
+    sorted_models = [m[0] for m in model_data]
+    sorted_values = [m[1] for m in model_data]
+    sorted_colors = [m[3] for m in model_data]
+    
+    sns.set_style("whitegrid")
+
+    plt.figure(figsize=(8, 5))
+    ax = sns.barplot(x=sorted_models, y=sorted_values, palette=sorted_colors)
+
+    plt.xlabel("Model", fontsize=14)
+    plt.ylabel("Value", fontsize=14)
+    plt.title("Model Means Differences Comparison", fontsize=16)
+    plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
+    plt.ylim(0, max(sorted_values) * 1.1)  
+
+    plt.subplots_adjust(bottom=0.2, left=0.15, right=0.9)
 
     plt.show()
--- text_feature_analysis/utils.py
+++ text_feature_analysis/utils.py
@@ -5,20 +5,37 @@ from scipy.stats import wilcoxon
 
 
 def prepare_dfs(
-    df: pd.DataFrame, text_feature_cols: List[str]
+    df: pd.DataFrame, text_feature_cols: List[str], df_name: str=None
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    female_df = df[df["gender"] == "female"][text_feature_cols].astype("float")
-    male_df = df[df["gender"] == "male"][text_feature_cols].astype("float")
+    available_cols = [col for col in text_feature_cols if col in df.columns]
+    missing_cols = set(text_feature_cols) - set(available_cols)
+
+    if missing_cols:
+        print(f"Missing columns: {missing_cols} in {df_name if df_name else 'unknown'}")  
+
+    female_df = df[df["gender"] == "female"][available_cols].astype("float")
+    male_df = df[df["gender"] == "male"][available_cols].astype("float")
 
     return female_df, male_df
 
 
+def get_available_cols(text_feature_cols: List[str], model_name: str, male_df: pd.DataFrame, female_df: pd.DataFrame):
+    available_cols = [col for col in text_feature_cols if col in male_df.columns and col in female_df.columns]
+    missing_cols = set(text_feature_cols) - set(available_cols)
+
+    if missing_cols:
+        print(f"Missing columns: {missing_cols} in {model_name if model_name else 'unknown'}")  
+
+    return available_cols
+
 def compute_wilcoxon_pvalues(
-    female_df: pd.DataFrame, male_df: pd.DataFrame, text_feature_cols: List[str]
+    female_df: pd.DataFrame, male_df: pd.DataFrame, text_feature_cols: List[str], model_name: str
 ) -> dict:
     wilcoxon_pvalues = dict()
 
-    for feature in text_feature_cols:
+    available_cols = get_available_cols(text_feature_cols, model_name, male_df, female_df)
+    
+    for feature in available_cols:
         res = wilcoxon(
             np.array(female_df[feature]),
             np.array(male_df[feature]),
@@ -37,12 +54,18 @@ def compute_wilcoxon_pvalues(
 
 
 def compute_mean_differences(
-    male_df: pd.DataFrame, female_df: pd.DataFrame, text_feature_cols: dict
+    male_df: pd.DataFrame, female_df: pd.DataFrame, text_feature_cols: dict, model_name: str=None
 ) -> dict:
-    mean_differences = {
-        col: male_df[col].mean() - female_df[col].mean() for col in text_feature_cols
-    }
+    available_cols = [col for col in text_feature_cols if col in male_df.columns and col in female_df.columns]
+    missing_cols = set(text_feature_cols) - set(available_cols)
 
+    if missing_cols:
+        print(f"Missing columns: {missing_cols} in {model_name if model_name else 'unknown'}")  
+
+    mean_differences = {
+        col: male_df[col].mean() - female_df[col].mean() for col in available_cols
+    }
+    
     normalized_differences = {
         col: mean_differences[col]
         / (
@@ -52,12 +75,15 @@ def compute_mean_differences(
         for col in mean_differences
     }
 
-    # Sort by absolute value
+    return normalized_differences
+
+
+def sort_dict_by_abs_value(mean_differences: dict) -> dict:
     sorted_means = dict(
         sorted(
             {
                 feature: value
-                for feature, value in normalized_differences.items()
+                for feature, value in mean_differences.items()
                 if not pd.isna(value)
             }.items(),
             key=lambda item: abs(item[1]),
@@ -66,3 +92,10 @@ def compute_mean_differences(
     )
 
     return sorted_means
+
+
+def get_sorted_mean_differences(male_df: pd.DataFrame, female_df: pd.DataFrame, text_feature_cols: dict, model_name: str=None) -> dict:
+    mean_differences = compute_mean_differences(male_df, female_df, text_feature_cols, model_name)
+    sorted_differences = sort_dict_by_abs_value(mean_differences)
+    
+    return sorted_differences
