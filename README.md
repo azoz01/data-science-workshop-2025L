5037158c4d4bb3db82f565c7f96950b3316a4689
Antoni Zajko <antoni.zajko.1@gmail.com>
2025-04-17 18:44:00 +0200

    Cleanup

---
+++ README.md
@@ -0,0 +1,20 @@
+### Prerequisities
+Install dependencies - `pip install -r requirements.txt`
+
+Place data into `data` directory under names:
+* `llm_dark_patterns.xls` - data with responses to feature-enriched prompts
+* `baseline` - data with baseline responses with no features
+### Raw text features analysis
+To reproduce these results run notebooks from directory `text_feature_analysis`. Order does not matter.
+
+### Linguistic text features analysis
+Calculate linguistic features using following script:
+``` bash
+cd linguistic_feature_analysis
+
+python calculate_linguistic_features_from_liwc.py --data ../data/LLM_all_enriched_baseline_02.xlsx -o ../data/processed
+
+python calculate_linguistic_features_from_liwc.py --data ../data/llm_dark_patterns.xls -o ../data/processed
+```
+
+And then run notebooks. Order does not matter.
--- text_feature_analysis/plots.py
+++ text_feature_analysis/plots.py
@@ -1,9 +1,10 @@
-from matplotlib import pyplot as plt
-import seaborn as sns
-import matplotlib.patches as mpatches
-import pandas as pd
-import numpy as np
 from typing import Tuple
+
+import matplotlib.patches as mpatches
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 group_colors = {
     "gpt": ("blue", 1),
@@ -11,21 +12,22 @@ group_colors = {
     "mixtral": ("green", 3),
     "mistral": ("green", 3),
     "qwen": ("red", 4),
-    "llama": ("purple", 5)
+    "llama": ("purple", 5),
 }
+
 
 def get_group_and_color(name: str) -> Tuple[int, str]:
     name = name.lower()
     for group, (color, order) in group_colors.items():
         if group in name:
             return order, color
-    return 99, "gray"  # Default if no match
+    return 1, "blue"  # Default if no match
 
 
 def plot_features_distribution(
     male_df: pd.DataFrame,
     female_df: pd.DataFrame,
-    baseline_df: pd. DataFrame,
+    baseline_df: pd.DataFrame,
     all_pvalues: dict,
     pvalue: float,
     model_name: str,
@@ -34,7 +36,7 @@ def plot_features_distribution(
     base_width: int = 3,
     base_height: int = 3,
     path_to_save: str = None,
-    show: bool = True
+    show: bool = True,
 ) -> None:
 
     # dynamically caluclate number of rows and columns
@@ -60,12 +62,8 @@ def plot_features_distribution(
         sns.kdeplot(
             baseline_df[col], label="baseline", fill=True, alpha=0.5, ax=ax, color="lightgrey"
         )
-        sns.kdeplot(
-            female_df[col], label="female", fill=True, alpha=0.5, ax=ax, color="hotpink"
-        )
-        sns.kdeplot(
-            male_df[col], label="male", fill=True, alpha=0.5, ax=ax, color="skyblue"
-        )
+        sns.kdeplot(female_df[col], label="female", fill=True, alpha=0.5, ax=ax, color="hotpink")
+        sns.kdeplot(male_df[col], label="male", fill=True, alpha=0.5, ax=ax, color="skyblue")
         ax.set_xlabel("Value")
         ax.set_ylabel("Density")
         ax.set_title(col)
@@ -79,9 +77,10 @@ def plot_features_distribution(
 
     # save and show
     if path_to_save:
-        plt.savefig(path_to_save+f'/{model_name}.png')
-        
+        plt.savefig(path_to_save + f"/{model_name}.png")
+
     plt.show() if show else plt.close()
+
 
 def plot_cohens_d_features(
     sorted_means: dict,
@@ -89,7 +88,7 @@ def plot_cohens_d_features(
     path_to_save: str = None,
     show: bool = True,
     across_models: bool = False,
-    model_grouping: bool = False
+    model_grouping: bool = False,
 ) -> None:
 
     if model_grouping:
@@ -104,7 +103,7 @@ def plot_cohens_d_features(
     else:
         sorted_features = list(sorted_means.keys())
         sorted_values = list(sorted_means.values())
-        
+
     sorted_colors = ["skyblue" if val > 0 else "hotpink" for val in sorted_values]
 
     # figure and plot
@@ -140,22 +139,25 @@ def plot_cohens_d_features(
 
     # save and show
     if path_to_save:
-        plt.savefig(path_to_save + f'/{model_name}.png')
+        plt.savefig(path_to_save + f"/{model_name}.png")
 
     plt.show() if show else plt.close()
 
-def plot_models_comparison(model_value_dict: dict, label_value: str, show: bool = True, path_to_save: str = None) -> None:
-    
+
+def plot_models_comparison(
+    model_value_dict: dict, label_value: str, show: bool = True, path_to_save: str = None
+) -> None:
+
     models = list(model_value_dict.keys())
     values = list(model_value_dict.values())
-    
+
     model_data = [(model, values[i], *get_group_and_color(model)) for i, model in enumerate(models)]
-    model_data.sort(key=lambda x: x[2])  
+    model_data.sort(key=lambda x: x[2])
 
     sorted_models = [m[0] for m in model_data]
     sorted_values = [m[1] for m in model_data]
     sorted_colors = [m[3] for m in model_data]
-    
+
     sns.set_style("whitegrid")
 
     plt.figure(figsize=(8, 5))
@@ -164,8 +166,8 @@ def plot_models_comparison(model_value_dict: dict, label_value: str, show: bool 
     plt.xlabel("Model")
     plt.ylabel(label_value)
     plt.title(f"{label_value} comparison across all models")
-    plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
-    plt.ylim(0, max(sorted_values) * 1.1)  
+    plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+    plt.ylim(0, max(sorted_values) * 1.1)
 
     plt.subplots_adjust(bottom=0.2, left=0.15, right=0.9)
 
@@ -173,6 +175,6 @@ def plot_models_comparison(model_value_dict: dict, label_value: str, show: bool 
     if path_to_save:
         plt.tight_layout()
         plot_title = label_value.lower().replace(" ", "_")
-        plt.savefig(path_to_save + f'/{plot_title}_total.png')
+        plt.savefig(path_to_save + f"/{plot_title}_total.png")
 
     plt.show() if show else plt.close()
